d0a69fb9dcec7e8f27dbf9e6e03b72f677b587e2
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-03 11:11:41 -0300

    build: :heavy_plus_sign: adiciona e configura o eslint

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <>
     <App />
-  </>
-);
+  </>,
+)
