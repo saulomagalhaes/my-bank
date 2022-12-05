3b0c38efc80b33c3163dbca825e56c2929962f51
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-05 10:09:14 -0300

    feat: :lipstick: adiciona responsividadeno form de cadastro

--- frontend/src/pages/RegisterUser/Form/index.tsx
+++ frontend/src/pages/RegisterUser/Form/index.tsx
@@ -22,9 +22,9 @@ export function Form() {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col items-center justify-center w-4/5 h-4/5 shadow-xl border rounded-lg bg-slate-200"
+      className="flex flex-col items-center justify-center py-4 px-4 shadow-xl border rounded-lg bg-slate-200 sm:w-3/5 sm:py-8 2xl:w-2/5 2xl:py-12"
     >
-      <img src={logo} alt="" className="w-40 h-30" />
+      <img src={logo} alt="" className="max-w-40 max-h-32" />
       <label htmlFor="name" className="text-slate-700">
         Nome
       </label>
@@ -34,7 +34,7 @@ export function Form() {
         value={name}
         placeholder="Digite seu nome"
         onChange={(event) => setName(event.target.value)}
-        className="border border-gray-300 rounded-md p-2 bg-slate-100"
+        className="border w-72 border-gray-300 rounded-md p-2 bg-slate-100"
       />
 
       <label htmlFor="email" className="text-slate-700">
@@ -46,7 +46,7 @@ export function Form() {
         value={email}
         placeholder="Digite seu e-mail"
         onChange={(event) => setEmail(event.target.value)}
-        className="border border-gray-300 rounded-md p-2 bg-slate-100"
+        className="border w-72 border-gray-300 rounded-md p-2 bg-slate-100"
       />
 
       <label htmlFor="password" className="text-slate-700">
@@ -58,7 +58,7 @@ export function Form() {
         value={password}
         placeholder="Digite uma senha"
         onChange={(event) => setPassword(event.target.value)}
-        className="border border-gray-300 rounded-md p-2 bg-slate-100"
+        className="border w-72 border-gray-300 rounded-md p-2 bg-slate-100 "
       />
 
       <button
