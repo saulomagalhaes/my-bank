fb640516824bba08664e81a1c5d708c2419dc268
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-12-04 21:16:59 -0300

    test: :white_check_mark: testa comportamento do botão cadastrar

---
+++ frontend/src/pages/RegisterUser/Form/Form.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { Form } from '.'
+
+describe('<Form />', () => {
+  it('o botão cadastrar deve estar desabilitado caso todos os campos não estejam preenchidos', () => {
+    render(<Form />)
+    const button = screen.getByRole('button', { name: /cadastrar/i })
+    expect(button).toBeDisabled()
+  })
+
+  it('o botão cadastrar deve estar habilitado caso todos os campos estejam preenchidos', () => {
+    render(<Form />)
+    const button = screen.getByRole('button', { name: /cadastrar/i })
+    const name = screen.getByPlaceholderText(/digite seu nome/i)
+    const email = screen.getByPlaceholderText(/digite seu e-mail/i)
+    const password = screen.getByPlaceholderText(/digite uma senha/i)
+
+    fireEvent.change(name, { target: { value: 'John Doe' } })
+    fireEvent.change(email, { target: { value: 'john@hotmail.com' } })
+    fireEvent.change(password, { target: { value: '123456' } })
+
+    expect(button).toBeEnabled()
+  })
+})
